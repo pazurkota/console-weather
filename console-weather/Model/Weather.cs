c4378ccb8e37735065019e76d0037065494d8e6b
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-10 10:45:22 +0200

    Modified "Weather" class

--- console-weather/Model/Weather.cs
+++ console-weather/Model/Weather.cs
@@ -1,23 +1,54 @@
-﻿namespace console_weather.Model; 
+﻿using Newtonsoft.Json;
 
-public class Weather {
-    public string Date { get; set; }
-    public string City { get; set; }
-    public decimal Temperature { get; set; } 
-    public string WeatherCondition { get; set; }
+namespace console_weather.Model;
+
+// Get location name and time
+public class Location {
+    public string Name { get; set; }
+    public string Country { get; set; }
+}
+
+public class Condition {
+    [JsonProperty("text")]
+    public string ConditionState { get; set; }
+}
+
+// Get current weather condition
+public class Current {
+    [JsonProperty("temp_c")]
+    public decimal Temperature { get; set; }
+    public Condition Condition { get; set; }
+    
+    [JsonProperty("wind_kph")]
     public decimal WindSpeed { get; set; }
+    
+    [JsonProperty("wind_dir")]
+    public string WindDirection { get; set; }
+    
+    [JsonProperty("pressure_mb")]
     public decimal Pressure { get; set; }
-    public string AlertHeadline { get; set; }
+    
+    [JsonProperty("humidity")]
+    public int Humidity { get; set; }
+    
+    [JsonProperty("cloud")]
+    public int Cloud { get; set; }
+}
+
+// Weather class itself
+public class Weather {
+    public Location Location;
+    public Current Current;
 
     public override string ToString() {
-        string str = $"Weather for {City} at {Date}:";
+        string str = $"Weather for {Location.Name}, {Location.Country} at {DateTime.Now}";
 
-        str += $"\n\nAverage Temperature: {Temperature}";
-        str += $"\nWeather Condition: {WeatherCondition}";
-        str += $"\nWind Speed: {WindSpeed}";
-        str += $"\nAir Pressure: {Pressure}";
-        str += $"\nAlert Headline: {AlertHeadline}";
-        
+        str += $"\n\nTeperature: {Current.Temperature}°C ({Current.Condition.ConditionState})";
+        str += $"\nWind Speed: {Current.WindSpeed}kmp ({Current.WindDirection})";
+        str += $"\nAir Pressure: {Current.Pressure} mbar";
+        str += $"\nHumidity: {Current.Humidity}%";
+        str += $"\nCloud Cover: {Current.Cloud}%";
+
         return str;
     }
 }
--- console-weather/Program.cs
+++ console-weather/Program.cs
@@ -1,7 +1,9 @@
 ﻿// "Weather" class example:
 using console_weather.Model;
+using Newtonsoft.Json;
 
 // Show API respond
 ApiData data = new ApiData();
 var strings = data.GetRequest();
-Console.WriteLine(strings);
+var json = JsonConvert.DeserializeObject<Weather>(strings);
+Console.WriteLine(json.ToString());
