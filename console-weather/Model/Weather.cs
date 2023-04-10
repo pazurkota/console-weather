ef272f11c4aaea53b80c5bae00a328ca5cfdf047
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-10 09:49:34 +0200

    Modified "Weather" class

--- console-weather/Model/Weather.cs
+++ console-weather/Model/Weather.cs
@@ -1,15 +1,23 @@
 ﻿namespace console_weather.Model; 
 
 public class Weather {
+    public string Date { get; set; }
     public string City { get; set; }
-    public int Temperature { get; set; } 
+    public decimal Temperature { get; set; } 
     public string WeatherCondition { get; set; }
+    public decimal WindSpeed { get; set; }
+    public decimal Pressure { get; set; }
     public string AlertHeadline { get; set; }
 
     public override string ToString() {
-        return $"Weather for: {City}" +
-               $"\nTemperature: {Temperature}°C" +
-               $"\nWeather Condition: {WeatherCondition}" +
-               $"\nAlert: {AlertHeadline}";
+        string str = $"Weather for {City} at {Date}";
+
+        str += $"\nAverage Temperature: {Temperature}";
+        str += $"\nWeather Condition: {WeatherCondition}";
+        str += $"\nWind Speed: {WindSpeed}";
+        str += $"\nAir Pressure: {Pressure}";
+        str += $"\nAlert Headline: {AlertHeadline}";
+        
+        return str;
     }
 }
