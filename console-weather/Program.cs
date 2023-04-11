b033d7c33710e936053b3faa56eb537fc79ecdd2
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-11 11:15:08 +0200

    Command line config

--- console-weather/Program.cs
+++ console-weather/Program.cs
@@ -1,9 +1,26 @@
-﻿// "Weather" class example:
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 
-using console_weather;
-using Newtonsoft.Json;
+public class Program {
+    public static async Task<int> Main(string[] args) {
+        var cityOption = new Option<string>(
+            new [] {"-c", "--city"},
+            "Get city name"
+        );
 
-// Show API respond
-ApiData data = new ApiData();
-var weather = data.ParseData();
-Console.WriteLine(weather.ToString());
+        var rootCommand = new RootCommand() {
+            cityOption
+        };
+        rootCommand.SetHandler(OnHandle, cityOption);
+
+        var commandLineBuilder = new CommandLineBuilder(rootCommand)
+            .UseDefaults();
+        var parser = commandLineBuilder.Build();
+
+        return await parser.InvokeAsync(args).ConfigureAwait(false);
+    }
+
+    private static void OnHandle(string str) => 
+        Console.WriteLine($"{str}");
+}
