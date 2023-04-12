9567c7838c13ad821a0482518bfc8f8dcc87cc86
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-12 19:44:38 +0200

    "GetApiKey()" error throw if file doens't exist

--- console-weather/API/ApiKeyHandler.cs
+++ console-weather/API/ApiKeyHandler.cs
@@ -1,5 +1,22 @@
-﻿namespace console_weather.API; 
+﻿using Newtonsoft.Json.Linq;
+
+namespace console_weather.API; 
 
 public class ApiKeyHandler {
-    
+    // Get API Key from config.json
+    public static string GetApiKey() {
+        var filePath = "config.json";
+        
+        // Check if file exist, and if not, throw error
+        if (!File.Exists(filePath)) {
+            throw new FileNotFoundException($"{filePath} has not been found!");
+        }
+        
+        var jsonText = File.ReadAllText(filePath);
+
+        JObject config = JObject.Parse(jsonText);
+
+        string key = config["api-key"].ToString();
+        return key;
+    }
 }
