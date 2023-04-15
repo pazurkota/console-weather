b2bbbff66ad56ebcba0ccca7b6893c56a95edd95
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-15 13:36:41 +0200

    gitflow-release-stash: v1.0

--- console-weather/Program.cs
+++ console-weather/Program.cs
@@ -4,10 +4,12 @@ using System.CommandLine.Parsing;
 using console_weather;
 using console_weather.API;
 
+namespace console_weather;
+
 public class Program {
     public static async Task<int> Main(string[] args) {
         var cityOption = new Option<string>(
-            new [] {"-c", "--city"},
+            new [] {"--c", "--city"},
             "Get city name"
         );
 
