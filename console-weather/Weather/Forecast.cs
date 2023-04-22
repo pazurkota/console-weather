a64827813df8480218c138310a65549d8883f072
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-22 21:24:36 +0200

    Created "Forecast" class

---
+++ console-weather/Weather/Forecast.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace console_weather.Weather; 
+
+public class Forecast {
+    [JsonProperty("forecastday")]
+    public Condition ForecastDay { get; set; }
+}
+
+public class Forecasts {
+    [JsonProperty("forecast")]
+    public List<Forecast> ForecastsDay { get; set; }
+}
