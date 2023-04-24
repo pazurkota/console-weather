c3ca01d56dd60f84c33793c504c243d8599af0c4
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-24 19:11:16 +0200

    Show forecast for the next day

--- console-weather/Weather/Forecast.cs
+++ console-weather/Weather/Forecast.cs
@@ -2,12 +2,21 @@
 
 namespace console_weather.Weather; 
 
-public class Forecast {
-    [JsonProperty("forecastday")]
-    public Condition ForecastDay { get; set; }
+public class ForecastDay {
+    [JsonProperty("date")] public string Date { get; set; } = null!;
+    [JsonProperty("day")] public Day Day { get; set; } = null!;
 }
 
-public class Forecasts {
-    [JsonProperty("forecast")]
-    public List<Forecast> ForecastsDay { get; set; }
+public class Day {
+    [JsonProperty("avgtemp_c")] public decimal AvgTemp { get; set; }
+    [JsonProperty("maxtemp_c")] public decimal MaxTemp { get; set; }
+    [JsonProperty("mintemp_c")] public decimal MinTemp { get; set; }
+    [JsonProperty("maxwind_kph")] public decimal MaxWindSpeed { get; set; }
+    [JsonProperty("daily_chance_of_rain")] public decimal ChanceOfRain { get; set; }
+    [JsonProperty("daily_chance_of_snow")] public decimal ChanceOfSnow { get; set; }
+    [JsonProperty("condition")] public Condition Condition { get; set; } = null!;
+}
+
+public class Forecast {
+    [JsonProperty("forecastday")] public List<ForecastDay> ForecastsDay { get; set; } = null!;
 }
