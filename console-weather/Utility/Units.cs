808d561b3daa785411d42cfdd14c860c800443c9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-04-25 19:56:38 +0200

    Created "Units" class

---
+++ console-weather/Utility/Units.cs
@@ -0,0 +1,7 @@
+namespace console_weather.Utility;
+
+public class Units {
+  public static string Degrees { get; set; }
+  public static string Speed { get; set; }
+  public static string Precipitation { get; set; }
+}
