f43473ab81483a0c2a6a8cd370a7e5726369fd37
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-03 18:09:20 +0200

    Changed default unit value to "UnitType.Eu"

--- console-weather/Utility/Settings.cs
+++ console-weather/Utility/Settings.cs
@@ -4,5 +4,5 @@ public struct Settings {
     public static string CityName { get; set; } = null!;
     public static bool DontShowAlerts { get; set; }
     public static bool ShowForecast { get; set; }
-    public static Units.UnitType Units { get; set; } = Utility.Units.UnitType.Si; // default
+    public static Units.UnitType Units { get; set; } = Utility.Units.UnitType.Eu; // default
 }
