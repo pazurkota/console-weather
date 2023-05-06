88cf4917c69c55f4f6cdc98d0503c061692d7da1
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-06 14:13:42 +0200

    Removed unused methods in "Units" class

--- console-weather/Utility/Units.cs
+++ console-weather/Utility/Units.cs
@@ -7,47 +7,4 @@ public class Units {
     public Units(UnitType unitType) {
         Unit = unitType;
     }
-
-    public string GetTempUnit() {
-        switch (Unit) {
-            case UnitType.Si:
-                return "°C";
-            case UnitType.Us:
-                return "°F";
-            case UnitType.Uk:
-                return "°C";
-            case UnitType.Eu:
-                return "°C";
-            default:
-                throw new ArgumentException("Invalid unit type");
-        }
-    }
-    
-    public string GetWindSpeedUnit() {
-        switch (Unit) {
-            case UnitType.Si:
-                return "m/s";
-            case UnitType.Us:
-                return "mph";
-            case UnitType.Uk:
-                return "mph";
-            case UnitType.Eu:
-                return "kph";
-            default:
-                throw new ArgumentException("Invalid unit type");
-        }
-    }
-    
-    public string GetPressureUnit() {
-        return "hPa";
-    }
-
-    public void SetUnitType(string unitStr) {
-        if (Enum.TryParse(unitStr, true, out UnitType unitType)) {
-            Unit = unitType;
-        }
-        else {
-            throw new ArgumentException("Invalid unit type");
-        }
-    }
 }
