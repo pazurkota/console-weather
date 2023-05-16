c15be14e361bfedc5b0752717185b1da51df296a
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-16 16:07:12 +0200

    Added properties into "AirQuality" class

--- console-weather/Weather/AirQuality.cs
+++ console-weather/Weather/AirQuality.cs
@@ -1,5 +1,13 @@
-﻿namespace console_weather.Weather; 
+﻿using Newtonsoft.Json;
+
+namespace console_weather.Weather; 
 
 public class AirQuality {
-    
+    [JsonProperty("co")] public decimal Co { get; set; }
+    [JsonProperty("o3")] public decimal O3 { get; set; }
+    [JsonProperty("no2")] public decimal No2 { get; set; }
+    [JsonProperty("so2")] public decimal So2 { get; set; }
+    [JsonProperty("pm2_5")] public decimal Pm25 { get; set; }
+    [JsonProperty("pm10")] public decimal Pm10 { get; set; }
+    [JsonProperty("us-epa-index")] public int UsEpaIndex { get; set; }
 }
