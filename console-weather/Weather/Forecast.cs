6c7813a726ad43e59e3e54ab0d6bf3bc5629075a
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-08-29 16:52:34 +0200

    Created "Astro" class

--- console-weather/Weather/Forecast.cs
+++ console-weather/Weather/Forecast.cs
@@ -4,6 +4,7 @@ namespace console_weather.Weather;
 
 public class ForecastDay {
     [JsonProperty("day")] public Day Day { get; set; } = null!;
+    [JsonProperty("astro")] public Astro Astro { get; set; } = null!;
 }
 
 public class Day {
@@ -33,6 +34,15 @@ public class Day {
     [JsonProperty("uv")] public decimal UvIndex { get; set; }
     
     [JsonProperty("condition")] public Condition Condition { get; set; } = null!;
+}
+
+public class Astro {
+    [JsonProperty("sunrise")] public string Sunrise { get; set; }
+    [JsonProperty("sunset")] public string Sunset { get; set; }
+    [JsonProperty("moonrise")] public string Moonrise { get; set; }
+    [JsonProperty("moonset")] public string Moonset { get; set; }
+    [JsonProperty("moon_phase")] public string MoonPhase { get; set; }
+    [JsonProperty("moon_illumination")] public string MoonIllumination { get; set; }
 }
 
 public class Forecast {
