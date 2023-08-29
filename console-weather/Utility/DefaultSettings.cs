f2e3c1e22d3321e760e625fc98d3e8c2f26d80ae
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-08-29 17:02:07 +0200

    Added "--astronomy" argument

--- console-weather/Utility/DefaultSettings.cs
+++ console-weather/Utility/DefaultSettings.cs
@@ -62,4 +62,11 @@ public class DefaultSettings {
         
         return cityName;
     }
+
+    public static bool ShowAstronomy() {
+        JObject jObject = JObject.Parse(GetJsonContent());
+        bool showAstronomy = jObject["show-astronomy"]?.ToObject<bool>() ?? false;
+        
+        return showAstronomy;
+    }
 }
