0542873efe7958f885c5d3e7d6c5f025645689a3
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-09-01 19:14:41 +0200

    Minor changes
    removed comment that are useless

--- console-weather/Weather/Alert.cs
+++ console-weather/Weather/Alert.cs
@@ -2,13 +2,11 @@
 
 namespace console_weather.Weather; 
 
-// A single Alert Class
 public class Alert {
     [JsonProperty("headline")] public string AlertHeadline { get; set; } = null!;
     [JsonProperty("desc")] public string AlertDescription { get; set; } = null!;
 }
 
-// A class for multiple weather alerts
 public class Alerts {
     [JsonProperty("alert")] public List<Alert> WeatherAlerts { get; set; } = null!;
 }
