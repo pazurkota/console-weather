b2a3eea17612faa6e9455841fb5610b90dbdb585
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-02-20 18:28:49 +0100

    Adds day and night ASCII weather icons

---
+++ console-weather/Icons/NightIcons.cs
@@ -0,0 +1,18 @@
+﻿namespace console_weather.Icons;
+
+public class NightIcons
+{
+    public static Dictionary<int, string> Icons = new()
+    {
+        {1000, "     :-=====--:                           \n" +
+               "  .-===-:::====:                          \n" +
+               " -==-.    -==:                            \n" +
+               ":==:     -==.                             \n" +
+               "===     .==-                              \n" +
+               "===     .==-                              \n" +
+               ":==:     -==.                             \n" +
+               " -==-.    -==:                            \n" +
+               "  .-===-:::====:                          \n" +
+               "     :-======-:                           "}
+    };
+}
